5dbbf90c3882145a918ec3bd853650828d84601c
tanzimminhaj777 <tanzimminhaj@gmail.com>
2026-07-18 20:46:19 +0600

    2nd nav bar added

--- src/components/shared/DarkMode.tsx
+++ src/components/shared/DarkMode.tsx
@@ -2,33 +2,32 @@ import { useEffect, useState } from "react";
 import { MdOutlineLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
 
-
-
 const DarkMode = () => {
-    const [active, setActive] = useState<boolean>(
-        localStorage.getItem("theme") === "dark"
-    )
-    
+  const [active, setActive] = useState<boolean>(
+    localStorage.getItem("proparty") === "dark",
+  );
 
-    useEffect(() => {
-        if(active){
-            document.documentElement.classList.add("dark")
-            localStorage.setItem('theme', 'dark')
-        } else{
-              document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-        }
-    }, [active])
-    
+  
+  useEffect(() => {
+    if (active) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("proparty", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("proparty", "light");
+    }
+  }, [active]);
+
   return (
-    <div onClick={() => {
-        setActive(!active)
-    }} className="text-2xl cursor-pointer">
-
-        {active? < MdDarkMode /> : <MdOutlineLightMode />}
-
+    <div
+      onClick={() => {
+        setActive(!active);
+      }}
+      className="text-2xl cursor-pointer"
+    >
+      {active ? <MdDarkMode /> : <MdOutlineLightMode />}
     </div>
-  )
-}
+  );
+};
 
-export default DarkMode
+export default DarkMode;
