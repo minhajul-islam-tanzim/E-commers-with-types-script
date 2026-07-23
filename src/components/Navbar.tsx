4e2dc7731ed62371bddb17cdb234b83b8dd14b0c
tanzimminhaj777 <tanzimminhaj@gmail.com>
2026-07-23 18:43:58 +0600

    responcive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,6 +19,7 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm ">
       <div className="w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16">
         {/* logo */}
+             
         <div className="flex items-center cursor-pointer opacity-65 hover:opacity-100 transition-all hover:scale-105 duration-300">
           <div className="w-4  h-4 bg-blue-600 rounded-full "></div>
           <div className="w-4 h-4 -ml-1 bg-red-600 rounded-full"></div>
@@ -39,19 +40,9 @@ const Navbar = () => {
           })}
         </div>
 
-        {/* get In touch box */}
-
-        <div>
-            <button className="bg-blue-700 px-4 py-2 shadow-lg rounded-full text-white">
-            <a href="#newsletterbutton">Get in touch</a>
-            </button>
-        </div>
-
-        {/* mobile menu */}
-
-            <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <button className="md:hidden p-2" onClick={() => setIsMenuOpen(!isMenuOpen)}>
           {
-            isMenuOpen ?  <IoCloseSharp />  : <IoMenu />
+            isMenuOpen ?  <IoCloseSharp className="size-6"/>  : <IoMenu className="size-6"/>
           }
             </button>
 
@@ -59,6 +50,40 @@ const Navbar = () => {
 
 
 
+
+
+        {/* get In touch box */}
+
+        <div className="md:block hidden">
+            <button className="bg-blue-500 px-4 py-2 shadow-sm rounded-full text-white hover:shadow-lg hover:bg-blue-700 cursor-pointer hover:scale-105 transition-all duration-200">
+            <a href="#newsletterbutton">Get in touch</a>
+            </button>
+        </div>
+
+        {/* mobile menu */}
+
+          {
+            isMenuOpen && (
+              <div className="md:hidden absolute top-full left-0 w-full bg-white shadow-md ">
+                <div className="container mx-auto px-4 space-y-3">
+                  {navLinks.map((link, index) => {
+                    return <a 
+                      onClick={(e) => {
+                        e.preventDefault()
+                        setActiveLink(link.href)
+                        setIsMenuOpen(false)
+                      }}
+                    className={`block py-2 font-medium ${activeLink === link.href ? 'text-blue-500' : 'text-gray-900' }`} key={index} href={link.href}>{link.label}</a>
+                  })}
+
+                     <button className="w-full bg-blue-500 px-4 py-2 shadow-sm rounded-full text-white hover:shadow-lg hover:bg-blue-700 cursor-pointer hover:scale-105 transition-all duration-200">
+            <a href="#newsletterbutton">Get in touch</a>
+            </button>
+
+                </div>
+              </div>
+            )
+          }
       </div>
     </nav>
   );
